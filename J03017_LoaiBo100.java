6261278f9f66f3124e99812ab5c47c95f723b3b3
HNamm <80162506+huydre@users.noreply.github.com>
2023-09-14 21:51:12 +0700

    Update

--- J03017_LoaiBo100.java
+++ J03017_LoaiBo100.java
@@ -1,16 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class J03017_LoaiBo100 {
+    public static class Pair {
+        public char first;
+        public int second;
+        Pair(char first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    public static int remove100(String str) {
+        ArrayList<Pair> arr = new ArrayList<>();
+        arr.add(new Pair('@',-1));
+        int maxlen = 0;
+        for (int i = 0 ; i < str.length(); i++) {
+            arr.add(new Pair(str.charAt(i),i));
+            while (arr.size() >= 3 &&
+                    arr.get(arr.size()-3).first=='1' &&
+                    arr.get(arr.size()-2).first=='0' &&
+                    arr.get(arr.size()-1).first=='0')
+            {
+                arr.remove(arr.size() - 3);
+                arr.remove(arr.size() - 2);
+                arr.remove(arr.size() - 1);
+            }
+            int tmp = arr.get(arr.size() - 1).second;
+            maxlen = Math.max(maxlen, i - tmp);
+        }
+        return maxlen;
+    }
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         int t = Integer.parseInt(cin.nextLine());
         while (t --> 0) {
-            String n = cin.next();
-            String m = n;
-            while (n.contains("100")) {
-                n = n.replace("100","");
-            }
-            System.out.println(m.length() - n.length());
+            String n = cin.nextLine();
+            System.out.println(remove100(n));
         }
     }
 }
--- J07045_LoaiPhong.java
+++ J07045_LoaiPhong.java
@@ -1,9 +1,44 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class J07045_LoaiPhong {
+    public static class LoaiPhong implements Comparable{
+        private String KyHieu, Ten, DonGia, PhiDichVu;
+
+        public LoaiPhong(String s) {
+            String[] arr = s.split(" ");
+            this.KyHieu = arr[0];
+            this.Ten = arr[1];
+            this.DonGia = arr[2];
+            this.PhiDichVu = arr[3];
+        }
+
+        public String getTen() {
+            return Ten;
+        }
+
+        @Override
+        public String toString() {
+            return KyHieu + " " + Ten + " " + DonGia + " " + PhiDichVu;
+        }
+
+        @Override
+        public int compareTo(Object obj) {
+            LoaiPhong lp = (LoaiPhong) obj;
+            return Ten.compareTo(lp.getTen());
+        }
+    }
     public static void main(String[] args) throws IOException {
-        Scanner cin = new Scanner(new File("DATA.in"));
+        ArrayList<LoaiPhong> ds = new ArrayList<>();
+        Scanner in = new Scanner(new File("PHONG.in"));
+        int n = Integer.parseInt(in.nextLine());
+        while(n-->0){
+            ds.add(new LoaiPhong(in.nextLine()));
+        }
+        Collections.sort(ds);
+        for(LoaiPhong tmp : ds){
+            System.out.println(tmp);
+        }
     }
 }
